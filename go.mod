020356375aefd0004332190b05fe8b9ea24e8477
sondrefjellving <sondrefjellving@gmail.com>
2025-01-07 11:40:45 +0100

    import dog module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sondrefjellving/puppy
 
 go 1.23.1
+
+require github.com/sondrefjellving/dog v0.0.0-20250107102316-c28e13f126ea
--- puppy.go
+++ puppy.go
@@ -1,9 +1,27 @@
 package puppy
 
+import (
+	"fmt"
+
+	"github.com/sondrefjellving/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func PrintDogCreds(name, race string, age int) {
+	gunnarTheDog := dog.Dog{
+		Name: "Gunnar",
+		Race: "Chihuahua",
+		Age: 1,
+	}
+	
+	fmt.Println(dog.Dog.GetDogCredentials(gunnarTheDog))
+	fmt.Printf("%s barks: %s", gunnarTheDog.Name, Barks())
+	fmt.Printf("%s jumps: %s", gunnarTheDog.Name, dog.Jump())
 }
